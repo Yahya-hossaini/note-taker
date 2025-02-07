95d876369c304be10a5e157a6a996bf57d1d2d08
Yahya Hossaini <yahyahossaini2024@gmail.com>
2025-02-07 15:23:43 +0430

    Server set ups:
    sending data to server.
    receiving data from server.

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://qoocexjshrajbffllgpw.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvb2NleGpzaHJhamJmZmxsZ3B3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4Mzk0MjEsImV4cCI6MjA1NDQxNTQyMX0.WlMiclnb_mcmeOnSFkw1t8fL-Brc5zpIGp6oRlWONFs',
+  );
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage()
-    );
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Flutter Demo', home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>{
+class _MyHomePageState extends State<MyHomePage> {
+  final _noteStream =
+      Supabase.instance.client.from('notes').stream(primaryKey: ['id']);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Notes'),
       ),
-      body: Container(),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _noteStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final notes = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(notes[index]['body']),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: ((context) {
+              return SimpleDialog(
+                title: const Text('Add a Note'),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                children: [
+                  TextFormField(
+                    onFieldSubmitted: (value) async {
+                      await Supabase.instance.client
+                          .from('notes')
+                          .insert({'body': value});
+                    },
+                  ),
+                ],
+              );
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
